147ff027602fe25774b97b6d43f8689447546cec
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-11-10 00:01:36 +0000

    Add code for two models and last exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Kermit", last_name: "the Frog", hourly_rate: 50)
+@store1.employees.create(first_name: "Waldo", last_name: "Find", hourly_rate: 65)
+@store2.employees.create(first_name: "Jessica", last_name: "Rabbit", hourly_rate: 45)
+@store1.employees.create(first_name: "Mayor", last_name: "Townsville", hourly_rate: 100)
+@store2.employees.create(first_name: "Ash", last_name: "Ketchum", hourly_rate: 70)
+@store1.employees.create(first_name: "Victor", last_name: "Frankenstein", hourly_rate: 75)
+@store2.employees.create(first_name: "Ham", last_name: "Burglar", hourly_rate: 90)
